a337a09fd78c50d8fefb92a33e3b802f08faf5ff
mazenghasham77-jpg <mazenghasham77@gmail.com>
2026-04-08 02:17:47 +0400

    fix server router import

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { env } from "./config/env";
-import { router } from "./routes";
+import  router  from "./routes";
 
 const app = express();
 
