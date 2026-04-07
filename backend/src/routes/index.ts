7c26d69fd0d674f5c271eb228ab636439554ea8c
mazenghasham77-jpg <mazenghasham77@gmail.com>
2026-04-08 02:39:19 +0400

    fix journal controller imports

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,7 +3,7 @@ import { login } from "../controllers/auth.controller";
 import { getMyCompany } from "../controllers/company.controller";
 import { listAccounts } from "../controllers/account.controller";
 import { authenticate } from "../middleware/auth";
-import { createJournal, listJournalEntries } from "../controllers/journal.controller";
+import { createJournalEntryHandler } from "../controllers/journal.controller";
 import { createInvoice, listInvoices } from "../controllers/invoice.controller";
 import { createBill, listBills } from "../controllers/bill.controller";
 import { createPayment, listPayments } from "../controllers/payment.controller";
@@ -26,7 +26,7 @@ router.get("/company/me", getMyCompany);
 router.get("/accounts", listAccounts);
 
 router.get("/journal-entries", listJournalEntries);
-router.post("/journal-entries", createJournal);
+router.post("/journal-entries", createJournalEntryHandler);
 
 router.get("/invoices", listInvoices);
 router.post("/invoices", createInvoice);
